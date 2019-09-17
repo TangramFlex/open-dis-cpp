b0a571b5a408fb027f956f70330948fd13046587
Joseph Richardson <joe.richardson@tangramflex.com>
2019-09-17 09:38:56 -0400

    Modified instructions

--- install_premake5.sh
+++ install_premake5.sh
@@ -4,6 +4,6 @@ cd tmp/
 wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-src.zip -O premake.zip
 unzip premake.zip
 cd premake-5.0.0-alpha6/build/gmake.unix
-make config=release
+make -j8 config=release
 cd ../../..
-mv premake-5.0.0-alpha6/bin/release/premake5 ~/bin/premake5 # Move premake5 to lib dir
+mv premake-5.0.0-alpha6/bin/release/premake5 usr/local/bin/premake5 # Move premake5 to lib dir

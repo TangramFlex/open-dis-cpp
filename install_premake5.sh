315cbf040f58bba1ed06f73dee870df846006d63
Joseph Richardson <joe.richardson@tangramflex.com>
2019-09-17 09:44:46 -0400

    modified move command

--- install_premake5.sh
+++ install_premake5.sh
@@ -6,6 +6,5 @@ unzip premake.zip
 cd premake-5.0.0-alpha6/build/gmake.unix
 make -j8 config=release
 cd ../../..
-ls premake-5.0.0-alpha6/bin/release;
-echo $PATH
-mv premake-5.0.0-alpha6/bin/release/premake5 usr/local/bin/ # Move premake5 to lib dir
+mkdir -p /usr/local/bin
+mv premake-5.0.0-alpha6/bin/release/premake5 /usr/local/bin/premake5 # Move premake5 to lib dir
